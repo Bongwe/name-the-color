1b3e64fa16c3a62ae30fd2d548b9aeed6e079b1e
bongwe <obomvu@gmail.com>
2018-11-11 19:24:16 +0200

    got the basic game working

--- js/main.js
+++ js/main.js
@@ -1,58 +1,65 @@
 
 
-startGame();
+let min = 0;
+let max = 4;
+let indexOfNextColor = 0;
+let numberOfColors = 4;
+let listOfAllColors = ["Green", "Red", "Silver", "Blue","Aquamarine","Azure", "Purple", "Bisque"];
 
-function startGame(){
-	
-	let min = 0;
-	let max = 8;
-	let numberOfQuestions = 4;
-	let indexToStartSelecting = 0;
-	
-	let questionCount = 0;
-	let listOfColors = ["Green", "Red", "Yellow", "Blue","Aquamarine","Azure", "Beige,", "Bisque"];
-	let gameChosenColorIndexes = getAListOfRandomNumbers(indexToStartSelecting, indexToStartSelecting + numberOfQuestions)	
-	let listOfSelectedColors = chooseTheNextColorsToDisplay(indexToStartSelecting, listOfColors);
-	let gameChosenColor = listOfColors[gameChosenColorIndexes];
-	
-	displayColors(listOfSelectedColors);
-	
-	$("#findMoreColors").click(function() {
-		indexToStartSelecting = indexToStartSelecting + 4;
-		listOfSelectedColors = chooseTheNextColorsToDisplay(indexToStartSelecting, listOfColors);
-		displayColors(listOfSelectedColors);
-	});
+playGame();
 
-	//this checks if the colors were clicked
-	for (let index = 0; index < 4; index++) {
+function playGame() {
+	listOfColorsToDisplay = chooseTheNextColorsToDisplay(indexOfNextColor, numberOfColors,  listOfAllColors);
+	gameChosenColor = listOfColorsToDisplay[getARandomNumber(min, max)];
+	displayColors(listOfColorsToDisplay, numberOfColors);
+	displayQuestion(gameChosenColor);
+	doWhenColorIsClicked(listOfColorsToDisplay, numberOfColors, gameChosenColor);
+	indexOfNextColor = indexOfNextColor + numberOfColors;
+}
+
+$("#findMoreColors").click(function(){
+	playGame();
+});
+
+function chooseTheNextColorsToDisplay(indexOfNextColor, numberOfColors, listOfAllColors) {
+	let chosenColors = [];
+	for (let index = 0; index < numberOfColors; index++) {
+		chosenColors [index] = listOfAllColors[index + indexOfNextColor];
+	}
+	return chosenColors;	
+}
+
+function displayColors(listOfColors, numberOfColors){
+	for (let index = 0; index < numberOfColors; index++) {
+		$("#block-" + index).css({
+			"background-color": listOfColors[index]
+		});
+	}
+}
+
+function doWhenColorIsClicked(listOfColors, numberOfColors,gameChosenColor ){
+	for (let index = 0; index < numberOfColors; index++) {
 		$("#block-"+index).click(function(){
-			let answer = listOfSelectedColors[index];
+			let answer = listOfColors[index];
 			checkTheAnswer(gameChosenColor, answer);
 		});
 	}
-
-	$("#theQuestion").html("Which of the above colours is: " + gameChosenColor + "?")
 }
 
-function displayColors(listOfSelectedColors){
-	for (let index = 0; index < 4; index++) {
-		$("#block-" + index).css({
-			"background-color": listOfSelectedColors[index]
-		});
-	}
+function displayQuestion(gameChosenColor) {
+	$("#theQuestion").html("Which of the above colors is " + gameChosenColor.toLowerCase() + "?");
 }
 
 function checkTheAnswer(gameChosenColor, userChosenAnswer){
 	if(userChosenAnswer == gameChosenColor) {
-		$("#theAnswer").html("Yes! that is correct!");
+		$("#theAnswer").html("Yes! that is " + gameChosenColor.toLowerCase() + ".");
 		userFoundTheColor = true;
 	} else {
-		$("#theAnswer").html("No, unfortunately you are wrong.");
+		$("#theAnswer").html("No that is " + userChosenAnswer.toLowerCase() + ".");
 	}
 }
 
 function getAListOfRandomNumbers(min, max){
-	//Return a random number between min and max:
 	let randoms = [];
 	let randomNumbers = [];
 
@@ -63,20 +70,9 @@ function getAListOfRandomNumbers(min, max){
 	}
 
 	return randomNumbers;
-	//document.write(randomNumberForBlock);
 }
 
-function getARandomNumber(){
-	let min = 0;
-	let max = 4;
+function getARandomNumber(min, max){
 	var randomNumber = Math.floor(Math.random()*max) + min;
 	return randomNumber;
-}
-
-function chooseTheNextColorsToDisplay(indexToStartSelecting, listOfColors){
-	let chosenColors = [];
-	for (let index = 0; index < 4; index++) {
-		chosenColors[index] = listOfColors[indexToStartSelecting + index];
-	}
-	return chosenColors;
 }
